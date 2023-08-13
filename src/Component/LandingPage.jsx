af6458d01b2afd4a8eda45d96d980ee351a6cc15
Sushmita <sushmitakumari363@gmail.com>
2023-08-13 08:49:38 +0530

    first commit

---
+++ src/Component/LandingPage.jsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export const LandingPage = () => {
+  return (
+    <Box>
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+    </Box>
+  );
+};
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import { LandingPage } from "../Component/LandingPage";
+
+export const HomePage = () => {
+  return (
+    <Box>
+      <LandingPage />
+    </Box>
+  );
+};
